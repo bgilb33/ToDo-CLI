978ed636c6760bb82f428b7ad0d14b63dd2ecd78
bgilb33 <bgilb33@bu.edu>
2025-04-25 16:07:28 -0400

    Added for mac

---
+++ run-peer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run the ToDo peer container, connecting to host machine's Redis/Postgres
+
+docker run --rm -it \
+  -e DB_HOST=host.docker.internal \
+  -e DB_USER=todo_user \
+  -e DB_PASSWORD=todo_pass \
+  -e DB_NAME=todo_db \
+  -e REDIS_HOST=host.docker.internal \
+  todo-peer-image
