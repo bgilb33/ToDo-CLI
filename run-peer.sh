72d222b6d9e77ef791494473240b591d70b13618
bgilb33 <bgilb33@bu.edu>
2025-04-27 15:52:38 -0400

    Added non-localhost

--- run-peer.sh
+++ run-peer.sh
@@ -3,9 +3,9 @@
 # Run the ToDo peer container, connecting to host machine's Redis/Postgres
 
 docker run --rm -it \
-  -e DB_HOST=host.docker.internal \
+  -e DB_HOST=10.0.0.249 \
   -e DB_USER=todo_user \
   -e DB_PASSWORD=todo_pass \
   -e DB_NAME=todo_db \
-  -e REDIS_HOST=host.docker.internal \
+  -e REDIS_HOST=10.0.0.249 \
   todo-peer-image
